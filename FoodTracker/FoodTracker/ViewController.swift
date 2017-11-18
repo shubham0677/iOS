e47bea40b4ee3928ce80e4e519c1456a75cfa157
Shubham Sharma <shubham.sharma0677@gmail.com>
2017-11-18 17:37:10 -0500

    Added views to main storyboard

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Shubham Sharma on 2017-11-17.
+//  Copyright © 2017 Shubham Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
