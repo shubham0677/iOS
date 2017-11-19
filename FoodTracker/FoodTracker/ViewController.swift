2a72c988cb898e31392da006266a321729a83011
Shubham Sharma <shubham.sharma0677@gmail.com>
2017-11-18 19:06:37 -0500

    Set delegate for text field and update label when text entered in text field

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
@@ -16,7 +16,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        nameTextField.delegate = self
+    }
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
     }
     
     //MARK: Actions
