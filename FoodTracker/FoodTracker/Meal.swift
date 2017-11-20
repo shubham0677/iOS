7cb989ceface6ecee883c3c543fa7fc908ba6129
Shubham Sharma <shubham.sharma0677@gmail.com>
2017-11-20 10:52:54 -0500

    Add checks to Meal model's initializer
    If no name of negative rating is provided to Meal class's initialzer, it should and return nil. Also, this makes the initializer a "failable initializer".

--- FoodTracker/FoodTracker/Meal.swift
+++ FoodTracker/FoodTracker/Meal.swift
@@ -16,7 +16,12 @@ class Meal {
     var photo: UIImage?
     var rating: Int
     
-    init(name: String, photo: UIImage?, rating: Int) {
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
         
         // Initialize stored properties.
         self.name = name
