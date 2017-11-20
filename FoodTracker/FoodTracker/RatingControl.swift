94d305d3c36cd03b08771a14e1214bd15349a4fa
Shubham Sharma <shubham.sharma0677@gmail.com>
2017-11-20 09:52:11 -0500

    Update rating value and button appearance when any star (rating button) is tapped

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -13,7 +13,11 @@ import UIKit
     //MARK: Properties
     private var ratingButtons = [UIButton]()
     
-    var rating = 0
+    var rating = 0 {
+        didSet {
+            updateButtonSelectionStates()
+        }
+    }
     
     @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
         didSet {
@@ -35,7 +39,20 @@ import UIKit
     
     //MARK: Button Action
     func ratingButtonTapped(button: UIButton) {
-        print("Button Pressed")
+        guard let index = ratingButtons.index(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            // if the selected star represents the current rating, reset the rating to 0.
+            rating = 0
+        } else {
+            // otherwise set the rating to the selected star
+            rating = selectedRating
+        }
     }
     
     required init(coder: NSCoder) {
@@ -83,6 +100,15 @@ import UIKit
             
             // Add the new button to the rating button array
             ratingButtons.append(button)
+        }
+        
+        updateButtonSelectionStates()
+    }
+    
+    private func updateButtonSelectionStates() {
+        for(index, button) in ratingButtons.enumerated() {
+            // If the index of a button is less than the rating, that button should be selected.
+            button.isSelected = index < rating
         }
     }
 }
